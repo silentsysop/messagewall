78c9bf61eaaa1eb4e8829a7a949f422b75f2d73e
Tuomas Puro <tuomas.puro@esedu.fi>
2024-10-08 11:34:35 +0300

    socket path fix

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -2,6 +2,8 @@
 import io from 'socket.io-client';
 import config from '../config';
 
-const socket = io(config.socketUrl);
+const socket = io(config.socketUrl,{
+  path: process.env.REACT_APP_SOCKET_PATH || '/socket.io',
+});
 
 export default socket;
